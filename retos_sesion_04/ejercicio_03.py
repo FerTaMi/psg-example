027ffab8d7b174a248d1b57cf057a0a7c41585d3
FerTaMi <fertancaram@gmail.com>
2025-05-17 15:08:19 -0400

    Ejercicio_03

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,19 @@
+ # Reglas del juego Monopolio
+print("REGLAS DEL MONOPOLY")
+print("1.PREPARACIÓN:")
+print("   * Cada jugador elige una ficha y recibe $1500.")
+print("   * Se colocan las propiedades y cartas en sus zonas.")
+
+print("2. TURNO DE JUGADOR:")
+print("   - Lanza los dados y avanza la ficha.")
+print("   - Si caes en propiedad sin dueño, puedes comprarla.")
+print("   - Si es de otro jugador, pagas alquiler.")
+
+print("3. CONSTRUCCIÓN:")
+print("   * Con todas las propiedades de un color, compras casas/hoteles.")
+print("   *Esto aumenta el alquiler que otros pagan.")
+
+print("4. BANCARROTA:")
+print("   *Si no puedes pagar, vendes propiedades o quedas eliminado.")
+
+print("¡Gana el último jugador en quedar con plata!")
