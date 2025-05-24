21c7dd377fc2b3a8eda7ffe2308b0f1f3fb59cc8
FerTaMi <fertancaram@gmail.com>
2025-05-24 06:12:14 -0400

    ejercicio 2

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,11 @@
+fahrenheit = 25
+celsius = (fahrenheit - 32) * 5 / 9
+print(celsius)
+
+fahrenheit = 300
+celsius = (fahrenheit - 32) * 5 / 9
+print(celsius)
+
+fahrenheit = 76
+celsius = (fahrenheit - 32) * 5 / 9
+print(celsius)
