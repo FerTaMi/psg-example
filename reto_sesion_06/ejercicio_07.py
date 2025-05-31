745a7792ace1ae7ce4f944c2715d15ba8dc1073b
FerTaMi <fertancaram@gmail.com>
2025-05-31 04:13:42 -0400

    ejercicio 7

---
+++ reto_sesion_06/ejercicio_07.py
@@ -0,0 +1,11 @@
+  #ejercicio 7
+
+cuadrado = 2 * 2
+suma = 2 + 2
+
+son_iguales = cuadrado == suma
+
+print("ejercicio 7")
+print("El cuadrado de 2 es:", cuadrado)
+print("La suma de 2 + 2 es:", suma)
+print("¿iguales?", son_iguales)
