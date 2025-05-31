9057b945f60e5536602ed548ae71532c1870f102
FerTaMi <fertancaram@gmail.com>
2025-05-31 03:50:13 -0400

    ejercicio 4

---
+++ reto_sesion_06/ejercicio_04.py
@@ -0,0 +1,11 @@
+# ejercicio_04
+numero1 = 17
+numero2 = 9
+resta = numero1 - numero2
+mitad = resta / 2
+es_par = mitad == int(mitad)
+
+print("ejercicio 4")
+print("La resta de", numero1, "y", numero2, "es:", resta)
+print("¿Es un número par?", es_par)
+print("")
