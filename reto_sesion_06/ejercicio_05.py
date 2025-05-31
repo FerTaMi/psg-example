96da94e56c82504c2401fbbf5a7bd3a648ea27e9
FerTaMi <fertancaram@gmail.com>
2025-05-31 03:54:54 -0400

    ejercicio 5

---
+++ reto_sesion_06/ejercicio_05.py
@@ -0,0 +1,26 @@
+#ejercicio 5
+n1 = 44
+n2 = 98
+
+par1 = int(n1 / 2) == (n1 / 2)
+par2 = int(n2 / 2) == (n2 / 2)
+
+mismo_paridad_1 = (par1 == par2)
+
+print("Primer par: 44 y 98")
+print("¿Ambos con la misma paridad?", mismo_paridad_1)
+print("")
+
+#para 111 y 333
+
+n3 = 111
+n4 = 333
+
+par3 = int(n3 / 2) == (n3 / 2)
+par4 = int(n4 / 2) == (n4 / 2)
+
+mismo_paridad_2 = (par3 == par4)
+
+print("Segundo par: 111 y 333")
+print("¿Ambos con la misma paridad?", mismo_paridad_2)
+
