0c7d9b98cf0453c752e276660288f8fbcf3f2cdb
FerTaMi <fertancaram@gmail.com>
2025-06-07 13:26:08 -0400

    ejercicio 2

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,8 @@
+
+a = int(input("Escribe el primer número: "))
+b = int(input("Escribe el segundo número: "))
+c = int(input("Escribe el tercer número: "))
+
+resultado = a - b - c
+
+print("El resultado de la resta es:", resultado)
