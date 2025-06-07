20b7a53f28d8010f6014938d19b03c605e475c82
FerTaMi <fertancaram@gmail.com>
2025-06-07 13:46:53 -0400

    ejercicio 6

---
+++ retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,15 @@
+frase = "fair reina de Bolivia"
+print(frase.title())
+
+
+texto = "Hola FAIR"
+print(texto.startswith("Hola"))
+
+texto = "Aprendiendo con FAIR"
+print(texto.endswith("FAIR"))
+
+mensaje = "lalala me encanta programar lalala"
+print(mensaje.count("la")) 
+
+mensaje = "Me gusta viajar"
+print(mensaje.replace("Java", "Python")) 
