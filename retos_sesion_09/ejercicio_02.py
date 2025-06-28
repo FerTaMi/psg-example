0ea92a11af5c9645dd231cf4ec1658c7d19a6f16
FerTaMi <fertancaram@gmail.com>
2025-06-28 05:09:49 -0400

    ejercicio_02.py

---
+++ retos_sesion_09/ejercicio_02.py
@@ -0,0 +1,6 @@
+numeritos = [5, 4, 3, 2, 2, 2, 0, 0, 1, 2]
+
+sublista_saltitos = numeritos[::-3]
+
+print("🎯 Lista original:", numeritos)
+print("🔄 Sublista inversa (saltando de 3 en 3):", sublista_saltitos)
