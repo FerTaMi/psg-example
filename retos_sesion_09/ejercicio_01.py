c9476628d30a2bf25671a48e9210d17b0529ce35
FerTaMi <fertancaram@gmail.com>
2025-06-28 05:02:26 -0400

    ejercicio_01.py

---
+++ retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,14 @@
+numeros = [1.5, 2.7, 3.3, 4.8, 5.9, 1, 2, 3, 4, 5]
+
+print("Lista de números con sus tipos:\n")
+
+print("Índice 0:", numeros[0], "-", type(numeros[0]))
+print("Índice 1:", numeros[1], "-", type(numeros[1]))
+print("Índice 2:", numeros[2], "-", type(numeros[2]))
+print("Índice 3:", numeros[3], "-", type(numeros[3]))
+print("Índice 4:", numeros[4], "-", type(numeros[4]))
+print("Índice 5:", numeros[5], "-", type(numeros[5]))
+print("Índice 6:", numeros[6], "-", type(numeros[6]))
+print("Índice 7:", numeros[7], "-", type(numeros[7]))
+print("Índice 8:", numeros[8], "-", type(numeros[8]))
+print("Índice 9:", numeros[9], "-", type(numeros[9]))
