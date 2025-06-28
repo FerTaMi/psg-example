5106a98e4be542283c495bff2c161e608dbda90c
FerTaMi <fertancaram@gmail.com>
2025-06-28 05:50:00 -0400

    ejercicio_04.py

--- retos_sesion_09/ejercicio_04.py
+++ retos_sesion_09/ejercicio_04.py
@@ -29,3 +29,34 @@ print("Precio de Oreo:", precio_oreo)
 print("Precio de Chizitos:", precio_chizitos)
 print("Producto más caro:", producto_max, "->", precio_max)
 print("Producto más barato:", producto_min, "->", precio_min)
+
+
+
+productos = ["Chocman", "Oreo", "Chizitos", "Trululu", "Galleta Dinosaurio", "Panditas"]
+precios = [1.5, 2.0, 1.2, 0.5, 2.3, 1.0]
+
+total_productos = len(productos)
+
+# ¿Cuánto cuestan todos los productos?
+total_precio = sum(precios)
+
+# Ordenar productos y precios del más barato al más caro
+productos_y_precios = list(zip(productos, precios))
+ordenados = sorted(productos_y_precios, key=lambda x: x[1])  # por precio
+
+# Separar listas 
+productos_ordenados = [item[0] for item in ordenados]
+precios_ordenados = [item[1] for item in ordenados]
+
+# Eliminar todos los productos de las listas
+productos.clear()
+precios.clear()
+
+# Resultados
+print("total de productos:", total_productos)
+print("Precio total de todos los productos:", total_precio)
+print("Productos ordenados del más barato al más caro:")
+print("Productos:", productos_ordenados)
+print("Precios:", precios_ordenados)
+print("productos después de limpiar la tienda:", productos)
+print("precios después de limpiar la tienda:", precios)
