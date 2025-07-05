826b1c53dad5f3a0143b70b6610f81dab2745352
FerTaMi <fertancaram@gmail.com>
2025-07-05 14:36:19 -0400

    Clase sesión 10

---
+++ sesion10.py
@@ -0,0 +1,426 @@
+conjunto = {'🍕','🍔','🍟','🌭'}
+print(conjunto) # {'🍔', '🌭', '🍕', '🍟'}
+conjunto.add('🥗')
+print(conjunto)  # {'🍔', '🍕', '🥗', '🍟', '🌭'}
+
+conjunto = {'🍕','🍔','🍟','🌭'}
+print(conjunto) # {'🍔', '🌭', '🍕', '🍟'}
+
+conjunto = {'🍕','🍔','🍟','🌭','🍕','🍟'}
+print(conjunto) # {'🍕', '🍟', '🌭', '🍔'}
+
+#Conjunto de enteros
+print ("Conjunto de enteros")
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto) 
+print(type(conjunto))
+
+print ("Conjunto de cadenas")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print(conjunto)
+print(type(conjunto))
+
+
+print ("Conjunto mixto")
+conjunto = {1, True, 3.14, '☕'}
+print(conjunto)
+print(type(conjunto))
+
+print ("Conjunto vacío")
+conjunto = set()
+print(conjunto)
+print(type(conjunto))
+
+print ("Conjunto a partir de la cadena")
+cadena = 'Hola Mundo'
+conjunto = set(cadena)
+print(conjunto)
+print(type(conjunto))
+
+print ("Conjunto a partir de una tupla")
+tupla = (1, 2, 3, 4, 5, 5)
+conjunto = set(tupla)
+print(conjunto)
+print(type(conjunto))
+
+print ("Conjunto a partir de una lista")
+lista = [True, False, 0, 1]
+conjunto = set(lista)
+print(conjunto)
+print(type(conjunto))
+
+print ("Conjunto por comprensión")
+conjunto = {x for x in '🍕🍔🍟🍕🍔🍟🍔🍟'}
+print(conjunto)
+print(type(conjunto))
+
+#Métodos de adición
+
+print ("Método add()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+conjunto.add('🥗')
+print(conjunto) 
+
+#update(valores)
+
+print ("Método update()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+conjunto.update(['🥤','🍦'])
+print(conjunto) 
+conjunto.update('🍩🍪')
+print(conjunto) 
+conjunto.update(('🍫','🍬'))
+print(conjunto)
+conjunto.update({'🍭','🍮'})
+print(conjunto)
+
+#Métodos de eliminación
+
+print ("Método remove()")
+conjunto = {'🍕','🍔','🍟','🌭'} 
+print (conjunto)
+conjunto.remove('🍔')
+print(conjunto)
+# conjunto.remove('🍔')
+# print(conjunto)
+# Key Error: '🍔'
+
+print ("Método discard()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+conjunto.discard('🍔')
+print(conjunto)
+conjunto.discard('🍔')
+print(conjunto)
+
+#pop()
+
+print ("Método pop()")
+conjunto = {'🍕','🍔','🍟','🌭', '🥤','🍦'}
+print (conjunto)
+print(conjunto.pop())
+print(conjunto)
+print(conjunto.pop())
+print(conjunto)
+
+#clear()
+
+print ("Método clear()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+conjunto.clear()
+print(conjunto)
+
+#Métodos de operaciones con conjunto
+#union()
+print ("Método union()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+union = conjunto1.union(conjunto2)
+print(union)
+
+#intersection(conjunto)
+
+print ("Método intersection()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+interseccion = conjunto1.intersection(conjunto2)
+print(interseccion)
+
+#difference(conjunto)
+
+print ("Método difference()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print ("1:",conjunto1, "2:",conjunto2)
+diferencia = conjunto1.difference(conjunto2)
+print("1 y 2:",diferencia)
+diferencia = conjunto2.difference(conjunto1)
+print("2 y 1:",diferencia)
+
+#symmetric_difference(conjunto)
+
+print ("Método symmetric_difference()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+diferencia_simetrica = conjunto1.symmetric_difference(conjunto2)
+print(diferencia_simetrica)
+
+#Métodos de asignación con operaciones
+
+print ("Método intersection_update()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+conjunto1.intersection_update(conjunto2)
+print(conjunto1)
+
+#difference_update(conjunto)
+
+print ("Método difference_update()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print ("1:",conjunto1, "2:",conjunto2)
+conjunto1.difference_update(conjunto2)
+print ("1:",conjunto1, "2:",conjunto2)
+
+#symmetric_difference_update(conjunto)
+
+print ("Método symmetric_difference_update()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+conjunto1.symmetric_difference_update(conjunto2)
+print(conjunto1)
+
+#Métodos de búsqueda
+
+#issubset(conjunto)
+print ("Método issubset()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+conjunto3 = {'🍔','🍟'}
+print (conjunto1, conjunto2,conjunto3)
+# ¿El conjunto1 es subconjunto del conjunto2?
+print(conjunto1.issubset(conjunto2))
+# ¿El conjunto3 es subconjunto del conjunto1?
+print(conjunto3.issubset(conjunto1))
+
+#issuperset(conjunto)
+print ("Método issuperset()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+conjunto3 = {'🍔','🍟'}
+print (conjunto1, conjunto2,conjunto3)
+# ¿El conjunto1 es superconjunto del conjunto2?
+print(conjunto1.issuperset(conjunto2)) # C1 contiene a C2?
+# ¿El conjunto1 es superconjunto del conjunto2?
+print(conjunto1.issuperset(conjunto3)) # C1 contiene a C3?
+
+#isdisjoint(conjunto)
+
+print ("Método isdisjoint()")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨'}
+conjunto3 = {'🍔','🍟'}
+print (conjunto1, conjunto2,conjunto3)
+# ¿El conjunto1 no tiene elementos en común con el conjunto2?
+print(conjunto1.isdisjoint(conjunto2))
+# ¿El conjunto1 no tiene elementos en común con el conjunto3?
+print(conjunto1.isdisjoint(conjunto3))
+
+#Métodos de copia
+
+print ("Asignación por referencia")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+copia = conjunto
+copia.add('🥗')
+print(conjunto)
+print(copia)
+
+#Para crear una copia de un conjunto se utiliza el método copy()
+
+print ("Método copy()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+copia = conjunto.copy()
+copia.add('🥗')
+print(conjunto)
+print(copia)
+
+#Funciones con conjuntos
+#len(conjunto)
+
+print ("Función len()")
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+print(len(conjunto))
+
+#max(conjunto)
+
+print ("Función max()")
+conjunto = {1, 2, 3, 4, 5}
+print (conjunto)
+print (max(conjunto))
+conjunto = {'🍕','🍔','🍟','🌭'}
+print (conjunto)
+print(max(conjunto))
+      
+#min(conjunto)
+
+print ("Función min()")
+conjunto = {1, 2, 3, 4, 5}
+print (conjunto)
+print (min(conjunto))
+conjunto = {'🍨','🍔','🍟','🍕'}
+print (conjunto)
+print(min(conjunto))
+
+#sum(conjunto)
+
+print ("Función sum()")
+conjunto = {1, 2, 3, 4, 5}
+print (conjunto)
+print (sum(conjunto))
+
+#Operadores con conjuntos
+#|= : Update
+
+print ("Operador |=")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨'}
+print (conjunto1, conjunto2)
+conjunto1 |= conjunto2
+print(conjunto1)
+
+#== compara si dos conjuntos son iguales
+
+print ("Operador ==")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍔','🍟', '🥤'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 == conjunto2)
+print(conjunto1 == conjunto3)
+
+#!= compara si dos conjuntos son diferentes
+print ("Operador !=")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍔','🍟', '🥤'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 != conjunto2)
+print(conjunto1 != conjunto3)
+
+#< compara si un conjunto es subconjunto y no igual a otro
+print ("Operador <")
+conjunto1 = {'🍔','🍟'}
+conjunto2 = {'🍔','🍟', '🥤'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 < conjunto2)
+print(conjunto1 < conjunto3)
+
+#> compara si un conjunto es superconjunto y no igual a otro
+print ("Operador >")
+conjunto1 = {'🍔','🍟','🥤','🍕'}
+conjunto2 = {'🍔','🍟', '🥤'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 > conjunto2)
+print(conjunto1 > conjunto3)
+
+#<= compara si un conjunto es subconjunto o igual a otro
+print ("Operador <=")
+conjunto1 = {'🍔','🍟'}
+conjunto2 = {'🍔','🍟'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 <= conjunto2)
+print(conjunto1 <= conjunto3)
+
+#>= compara si un conjunto es superconjunto o igual a otro
+print ("Operador >=")
+conjunto1 = {'🍔','🍟'}
+conjunto2 = {'🍔','🍟'}
+conjunto3 = {'🍕','🍨'}
+print (conjunto1, conjunto2, conjunto3)
+print(conjunto1 >= conjunto2)
+print(conjunto1 >= conjunto3)
+
+#Operadores para operaciones con conjuntos
+#| retorna la unión de dos conjuntos
+
+print ("Operador |")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+union = conjunto1 | conjunto2
+print(union)
+
+#& retorna la intersección de dos conjuntos
+print ("Operador &")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+interseccion = conjunto1 & conjunto2
+print(interseccion)
+
+#- retorna la diferencia de dos conjuntos
+print ("Operador -")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print ("1:",conjunto1, "2:",conjunto2)
+diferencia = conjunto1 - conjunto2
+print("1 - 2:",diferencia)
+diferencia = conjunto2 - conjunto1
+print("2 - 1:",diferencia)
+
+#^ retorna la diferencia simétrica de dos conjuntos
+print ("Operador ^")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+diferencia_simetrica = conjunto1 ^ conjunto2
+print(diferencia_simetrica)
+
+#|= recibe un conjunto y agrega al conjunto inicial los elementos del conjunto recibido
+
+print ("Operador |= Unión")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+conjunto1 |= conjunto2
+print(conjunto1)
+
+#&= recibe un conjunto y asigna al conjunto inicial la intersección de ambos conjuntos
+print ("Operador &= Intersección")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+conjunto1 &= conjunto2
+print(conjunto1)
+
+#-=
+print ("Operador -= Diferencia")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print ("1:",conjunto1, "2:",conjunto2)
+conjunto1 -= conjunto2
+print("1 - 2:",conjunto1)
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 -= conjunto1
+print("2 - 1:",conjunto2)
+
+#^=
+print ("Operador ^= Diferencia simétrica")
+conjunto1 = {'🍔','🍟', '🥤'}
+conjunto2 = {'🍕','🍨','🥤'}
+print (conjunto1, conjunto2)
+conjunto1 ^= conjunto2
+print(conjunto1)
+
+#Conjuntos inmutables
+#frozenset()
+conjunto = frozenset({'🍔','🍕','🥗','🍟','🌭'})
+print(conjunto)
+print(type(conjunto))
+
+
+conjunto = frozenset({1, 2, 3, 4, 5})
+print(conjunto)
+print(conjunto.add(6)) # AttributeError: 'frozenset' object has no attribute 'add'
+print(conjunto.remove(1)) # AttributeError: 'frozenset' object has no attribute 'remove'
+#print(conjunto |= {6}) # SyntaxError: invalid syntax
+
+
+print ("Conjunto de conjuntos")
+conjunto = {frozenset({'🍅','🍓','🍎'}), frozenset({'🍈','🍐','🍏'})}
+print(conjunto)
+print(type(conjunto))
