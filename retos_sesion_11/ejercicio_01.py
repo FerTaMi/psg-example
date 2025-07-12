23dee2b8aec3ca6773e88b30a62001a88d804b02
FerTaMi <fertancaram@gmail.com>
2025-07-11 22:13:49 -0400

    ejercicio_01.py

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,17 @@
+
+animal = {
+    "especie": "Pulpo",
+    "hábitat": "Océano Pacífico",
+    "dieta": "Crustáceos y peces pequeños",
+    "estado_de_salud": "Saludable",
+    "edad": 4
+}
+
+
+cuidadores = {"Lucía", "Carlos", "Mateo"}
+
+print("Información del animal marino:")
+print(animal)
+
+print("\nCuidadores responsables:")
+print(cuidadores)
