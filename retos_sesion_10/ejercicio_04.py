4ec4d414d4fdc44686a421e4dc1f5495e4c80c23
FerTaMi <fertancaram@gmail.com>
2025-08-16 03:57:00 -0400

    ejercicio_04.py

--- retos_sesion_10/ejercicio_04.py
+++ retos_sesion_10/ejercicio_04.py
@@ -1,16 +1,23 @@
-#postres favoritos de Jane y Jhon 🍰
-jane = {"Lemon Pie", "Brownie", "Tarta de Manzana", "Helado de Chocolate", "Flan"}
-jhon = {"Carrot Cake", "Croissant de Chocolate", "Lemon Pie", "Tarta de Manzana", "Pudding"}
+#5
+# Postres favoritos
+jane = ["Lemon Pie", "Brownie", "Tarta de Manzana", "Helado de Chocolate", "Flan"]
+jhon = ["Carrot Cake", "Croissant de Chocolate", "Lemon Pie", "Tarta de Manzana", "Pudding"]
 
-# ostres que tienen en común
-comunes = jane & jhon
-print("🍭 Postres en común:", comunes)
+# Buscamos postres que no tienen en común
+solo_jane = []
+for postre in jane:
+    if postre not in jhon:
+        solo_jane.append(postre)
 
-#valculamos compatibilidad (basado en Jane, que tiene 5 postres)
-porcentaje = (len(comunes) / len(jane)) * 100
+solo_jhon = []
+for postre in jhon:
+    if postre not in jane:
+        solo_jhon.append(postre)
 
-#el resultado de compatibilidad
-if porcentaje > 50:
-    print(f"💖 ¡Son compatibles! Tienen un {porcentaje:.0f}% de gustos en común.")
-else:
-    print(f"💔 Mmm... solo un {porcentaje:.0f}% en común. Hora de charlarlo 😅.")
+#Calculamos porcentaje con el total de postres de ambos
+diferentes = solo_jane + solo_jhon
+total = len(jane) + len(jhon)
+porcentaje = (len(diferentes) / total) * 100
+
+print("Postres diferentes:", diferentes)
+print("Porcentaje de diferencia:", porcentaje)
